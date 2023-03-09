acd4c501bb45bfcef20c38d8ed9d497bbae3e891
nyota1124 <nyota1124@gmail.com>
2023-03-09 11:43:08 +0900

    2023-03-09

--- pages/main.py
+++ pages/main.py
@@ -8,7 +8,10 @@ import requests
 import streamlit as st
 import pymysql
 import os 
+import streamlit as st
+import base64
 
+from PIL import Image
 from streamlit_autorefresh import st_autorefresh
 from st_on_hover_tabs import on_hover_tabs
 from streamlit_folium import st_folium
@@ -64,13 +67,12 @@ def load_data(idx:int):
     
     return result,result_weather,result_genday
 
-def weather_lottie_setfn(code:str):
-    if code=="맑음":
-        return  "./lotties/weather-sunny.json"
-    elif code=="구름 많음":
-        return  "./lotties/weather-verycloud.json"
-    elif code=="흐림":
-        return  "./lotties/weather-cloud.json"
+def render_svg(svg):
+    """Renders the given svg string."""
+    b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
+    html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+    st.write(html, unsafe_allow_html=True)
+    
 def main():
 	os_name =  platform.system()
 	os_dir = os.getcwd()
@@ -97,15 +99,24 @@ def main():
 					# Inject CSS with Markdown
 	st.markdown(hide_table_row_index, unsafe_allow_html=True)
 		
-	file_path = "./lotties/car-ani.json"
-	with open(file_path, 'r') as file:lottie_hello  = json.load(file)
+
 	result,result_weather,result_genday = load_data(164010)
+
 	# st.write(result)
 	# st.write(result_weather)
 	# st.write(result_genday)
 	# st.write(type(result)) 
 	with st.sidebar:
-		st_lottie(lottie_hello)
+		f = open("./lotties/sub-logo.svg", 'rt', encoding='UTF8')
+		lines = f.readlines()	
+		line_string=''.join(lines)
+
+		render_svg(line_string)
+		colored_header(
+			label=" ",
+			description = " ",
+			color_name="violet-70",
+		)
 		tabs = on_hover_tabs(tabName=['Dashboard', 'Graph', 'ALARM'], 
 							iconName=['dashboard', 'money', 'alarm'], default_choice=0)
 	if tabs == 'Dashboard':
@@ -124,19 +135,14 @@ def main():
 			col1,col2,col3= st.columns(3)
 			with col1:
 				with elements("meter"):
-					with mui.Paper(elevation=24, variant="outlined", square=True):
-						with mui.Button:
-							mui.icon.EmojiPeople()
-							mui.icon.DoubleArrow()
-							mui.Typography("계측기 정보")
-
+						st.write("계측기 정보")
 						gauge_option = {
 								"tooltip":{"formatter": "{a} <br/>{b} : {c}"},
 								"series": [{"name":"kw/H","type": "gauge","max":800,"pointer":{"icon":"path://M12.8,0.7l12,40.1H0.7L12.8,0.7z","length":"12%","width":20,"offsetCenter":[0,"-60%"],"itmestyle":{"color":"inherit"}},"progress":{"show": "true"},"detail":{"valueAnimation":"false","formmater":"{value}"},"data": [{"value":result[0][3],"name":"발전력"}],}]
 							}
 	
 						df1 = pd.DataFrame(
-							(("주파수",result[0][4]),("me_pf",result[0][5])),
+							(("오늘의 발전량",result_genday[0][3]),("주파수",result[0][4]),("me_pf",result[0][5])),
 							columns=("Name","Value"),
 						)
 						colu1,colu2 =st.columns(2)
@@ -180,6 +186,7 @@ def main():
 
 			with col2:
 				with st.container():
+					st.write("현황")
 					collu1,collu2,collu3,collu4,collu5 =st.columns(5)
 					with collu1:
 						file_path_solar = "./lotties/dash_solar.json"
@@ -210,9 +217,10 @@ def main():
 							with open(file_path_ondown, 'r') as file:lottie_direction = json.load(file)
 							st_lottie(lottie_direction,key="dash_ondown")
 						elif result[0][13] == 0 : 
-							file_path_onsleep = "./lotties/dash_onsleep.json"
-							with open(file_path_onsleep, 'r') as file:lottie_direction = json.load(file)
-							st_lottie(lottie_direction,key="dash_onsleep")
+							st.warning("대기중")
+							# file_path_onsleep = "./lotties/dash_onsleep.json"
+							# with open(file_path_onsleep, 'r') as file:lottie_direction = json.load(file)
+							# st_lottie(lottie_direction,key="dash_onsleep")
 
 						if result[0][13] > 0 :
 							file_path_uncharging = "./lotties/dash_uncharging.json"
@@ -280,14 +288,16 @@ def main():
 			col1,col2,col3 = st.columns(3)
 			with col1:
 				with st.container():
+					st.write("배터리정보")
 					colu1,colu2 = st.columns(2)
+					
 					with colu1:
 						liquidfill_option = {
 							"series": [{"type": "liquidFill", "data": [result[0][16]/100, result[0][16]/100*0.8, result[0][16]/100*0.6, result[0][16]/100*0.4],"shape":"container"}]
 						}
 						st_echarts(liquidfill_option)
 					with colu2:
-						st.write("배터리정보")
+						
 						df = pd.DataFrame(
 							(("Heartbeat",result[0][15]),
 							("SOH",result[0][17]),
@@ -331,13 +341,14 @@ def main():
 						st_echarts(options=options)
 			with col3:
 				with st.container():
+									st.write("imd")
 									colu1,colu2 = st.columns(2)
 									with colu1:
 										st.metric(label="센서1 온도(℃)", value=result[0][25] )
 
 										st.metric(label="센서2 온도(℃)", value=result[0][26] ) 
 									with colu2:
-										st.write("imd")
+
 										df = pd.DataFrame(
 											(("iso_v",result[0][23]),
 											("test_alarm",result[0][24])),
@@ -354,12 +365,12 @@ def main():
 										# Inject CSS with Markdown
 										st.markdown(hide_table_row_index, unsafe_allow_html=True)
 										st.table(df)
-		with st.container():
-			colored_header(
-				label="오늘의 발전량은 "+str(result_genday[0][3])+"kw 입니다.",
-				description=" ",
-				color_name="orange-80",
-			)
+		# with st.container():
+		# 	colored_header(
+		# 		label="오늘의 발전량은 "+str(result_genday[0][3])+"kw 입니다.",
+		# 		description=" ",
+		# 		color_name="orange-80",
+		# 	)
 # 		with st.container():
 # 			colu1,colu2=st.columns(2)
 # 			with colu1:
